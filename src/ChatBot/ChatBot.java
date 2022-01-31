f63a6124e18864f75120b82c3f7718d0a2ddc839
hoyajjjj <k.v.komarnytska@student.khai.edu>
2022-01-31 20:29:05 +0200

    ChatBot.java 1-st stage

---
+++ src/ChatBot/ChatBot.java
@@ -0,0 +1,10 @@
+package ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args){
+        String bot_name = "Karina";
+        int bot_year = 2022;
+        System.out.println("Hello! My name is " + bot_name + ".");
+        System.out.println("I was created in " + bot_year + ".");
+    }
+}
