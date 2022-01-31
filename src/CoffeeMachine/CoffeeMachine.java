ab5843505569f95c01f3d8a05670fd79a82b1276
hoyajjjj <k.v.komarnytska@student.khai.edu>
2022-01-31 21:22:16 +0200

    CoffeeMachine.java 4-th stage

--- src/CoffeeMachine/CoffeeMachine.java
+++ src/CoffeeMachine/CoffeeMachine.java
@@ -3,30 +3,100 @@ package CoffeeMachine;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    public static void main(String [] args){
+    int WATER = 400;
+    int MILK = 540;
+    int BEANS = 120;
+    int CUPS = 9;
+    int MONEY = 550;
 
-        Scanner Scanner = new Scanner(System.in);
-        System.out.print("Write how many ml of water the coffee machine has:");
-        int WATER = Scanner.nextInt();
-        System.out.print("Write how many ml of milk the coffee machine has:");
-        int MILK = Scanner.nextInt();
-        System.out.print("Write how many grams of coffee beans the coffee machine has:");
-        int BEANS = Scanner.nextInt();
-        System.out.print("Write how many cups of coffee you will need:");
-        int CUPS = Scanner.nextInt();
-        //
-        int F1 = WATER / 200;
-        int F2 = MILK / 50;
-        int F3 = BEANS / 15;
-        //
-        int Limitation = Math.min(F1, Math.min(F2, F3));
-        if (CUPS > Limitation) {
-            System.out.println("No, I can make only " + Limitation + " cup(s) of coffee");
-        } else if (CUPS == Limitation) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else {
-            int Availability = Limitation - CUPS;
-            System.out.println("Yes, I can make that amount of coffee (and even " + Availability + " more than that)");
+    public static void main(String[] args) {
+        CoffeeMachine main = new CoffeeMachine();
+        main.START();
+    }
+
+    public void START() {
+        Scanner in = new Scanner(System.in);
+        boolean isexit = false;
+        while (!isexit) {
+            System.out.println();
+            System.out.println("Write action (buy, fill, take, remaining, exit): ");
+            String action = in.next();
+            isexit = modifyState(action, in);
         }
+        in.close();
+    }
+
+    private void STANDART_AVAILABILITY() {
+        System.out.println("The coffee machine has: ");
+        System.out.println(this.WATER + " of water");
+        System.out.println(this.MILK + " of milk");
+        System.out.println(this.BEANS + " of coffee beans");
+        System.out.println(this.CUPS + " of disposable cups");
+        System.out.println("$" + this.MONEY + " of money");
+    }
+
+    private void COMP_ORDER(int water, int milk, int beans, int money) {
+        if (CUPS > 0 && this.WATER - water >= 0 && this.MILK - milk >= 0 && this.BEANS - beans >= 0) {
+            System.out.println("I have enough resources, making you a coffee!");
+            this.CUPS--;
+            this.WATER -= water;
+            this.MILK -= milk;
+            this.BEANS -= beans;
+            this.MONEY += money;
+        } else {
+            System.out.println("Sorry, not enough " +
+                    (CUPS <= 0 ? "cups" : this.WATER - water < 0 ? "water" : this.MILK - milk < 0 ? "milk" : "beans")
+                    + "!");
+        }
+    }
+
+
+    private boolean modifyState(String action, Scanner Scanner) {
+        System.out.println();
+        switch (action) {
+            case "buy":
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+                switch (Scanner.next()) {
+                    case "1":
+                        COMP_ORDER(250, 0, 16, 4);
+                        break;
+                    case "2":
+                        COMP_ORDER(350, 75, 20, 7);
+                        break;
+                    case "3":
+                        COMP_ORDER(200, 100, 12, 6);
+                        break;
+                    case "back":
+                        return false;
+                    default:
+                        break;
+                }
+                break;
+            case "fill":
+                System.out.println("Write how many ml of water do you want to add:");
+                this.WATER += Scanner.nextInt();
+                //
+                System.out.println("Write how many ml of milk do you want to add:");
+                this.MILK += Scanner.nextInt();
+                //
+                System.out.println("Write how many grams of coffee beans do you want to add:");
+                this.BEANS += Scanner.nextInt();
+                //
+                System.out.println("Write how many disposable cups of coffee do you want to add:");
+                this.CUPS += Scanner.nextInt();
+                break;
+            case "take":
+                System.out.println("I gave you " + this.MONEY + "$");
+                this.MONEY = 0;
+                break;
+            case "remaining":
+                this.STANDART_AVAILABILITY();
+                break;
+            case "exit":
+                return true;
+            default:
+                break;
+        }
+        return false;
     }
 }
